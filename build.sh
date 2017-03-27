bbd1deedd1398c307056f8bfa1b1b65fb7660645
Rahul Palake <rahul.palake@cuelogic.co.in>
2017-03-27 19:59:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 #mkdir $JENKINS_HOME/deployment1
 
-tar -cvzf $JENKINS_HOME/deployment/cuelab-$(date +\%d-\%m-\%Y-%H:%M:%S).tar.gz
+tar -cvzf $JENKINS_HOME/deployment/cuelab-$(date +\%d-\%m-\%Y-%H:%M:%S).tar.gz .
