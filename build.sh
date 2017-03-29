d60524a2296bc3a22360932f11084e6020549b62
Rahul Palake <rahul.palake@cuelogic.co.in>
2017-03-29 18:32:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 #mkdir $JENKINS_HOME/deployment1
 
-tar -cvzf $JENKINS_HOME/deployment/cuelab-$(date +\%d-\%m-\%Y-%H:%M:%S).tar.gz .
+#tar -cvzf $JENKINS_HOME/deployment/cuelab-$(date +\%d-\%m-\%Y-%H:%M:%S).tar.gz .
 
-ssh -i $AN_ACCESS_KEY ubuntu@54.194.154.163 "echo deploy success..!!"
+ssh -i $JENKINS_HOME/riot-demo.pem ubuntu@54.194.154.163 "echo deploy success..!!"
